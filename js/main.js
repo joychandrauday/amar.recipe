bae6ec3579bc4f6a64a2db1777eb71423023b7c6
joychandrauday <joychandraud@gmail.com>
2024-03-05 22:28:10 +0600

    dummy

--- js/main.js
+++ js/main.js
@@ -30,7 +30,16 @@ const loadRecipeSingle = async (category) => {
             <div class="card-body">
                 <h2 class="card-title">${recipe.strMeal}</h2>
                 <button class="btn btn-primary" onclick="recipeByID('${recipe.idMeal}')">Learn now!</button>
+                <button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>open modal</button>
+                <dialog id="my_modal_2" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">Press ESC key or click outside to close</p>
                 </div>
+                <form method="dialog" className="modal-backdrop"> 
+                    <button>close</button>
+                </form>
+                </dialog>
             </div>
         `
         randomRecipeContainer.appendChild(newDiv);
